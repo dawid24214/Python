77d0f59c3cad7fe66833eb08e37e8d161581df05
Dawid Grochowski <155809372+dawid24214@users.noreply.github.com>
2026-04-03 23:08:55 +0200

    Create Zmiana_wartości_zmiennej.py

---
+++ Zmiana_wartości_zmiennej.py
@@ -0,0 +1,10 @@
+punkty = 100
+addition_value = punkty + 20
+print(addition_value)
+
+subtraction_value = punkty - 20
+print(subtraction_value)
+
+multiplication_value = punkty * 10
+print(multiplication_value)
+
